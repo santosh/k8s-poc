2933755016715fd517a9b6beb6e779b684d861bb
Santosh Kumar <sntshkmr60@gmail.com>
2021-09-03 14:29:57 +0000

    Fibonaci microservice init

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <header>
+            <Link to="/">Home</Link>
+            <Link to="/otherpage">Other Page</Link>
+          </header>
+          <div>
+            <Route exact path="/" component={Fib} />
+            <Route exact path="/otherpage" component={OtherPage} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
