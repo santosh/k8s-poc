f75b25bee79ef867909c071c5a2d51836bad6595
Santosh Kumar <sntshkmr60@gmail.com>
2022-01-19 12:23:06 +0530

    Reoaganise

--- docker_push_and_k8s_deploy.sh
+++ docker_push_and_k8s_deploy.sh
@@ -23,3 +23,9 @@ do
 
     # echo ${TARGET_IMAGE_LATEST} --- ${TARGET_IMAGE_VERSIONED}
 done
+
+kubectl apply -f k8s
+
+kubectl set image deployments/server-deployment server=sntshk/complex-server:$SHA
+kubectl set image deployments/client-deployment client=sntshk/complex-client:$SHA
+kubectl set image deployments/worker-deployment worker=sntshk/complex-worker:$SHA
