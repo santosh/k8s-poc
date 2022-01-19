1ae95d49e47e1117365bb69f0b7bb182f9db2e9c
Santosh Kumar <sntshkmr60@gmail.com>
2022-01-19 12:52:52 +0530

    Add header to app

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
       <Router>
         <div className="App">
           <header>
+            <h1>Fibonacci Calculator on Kubernetes</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
